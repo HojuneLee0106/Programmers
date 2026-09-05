597b99111c8346fb46be018f7726b1a7804a676a
HojuneLee <147648712+HojuneLee0106@users.noreply.github.com>
2026-09-05 17:51:21 +0900

    [level 2] Title: 2 x n 타일링, Time: 8.56 ms, Memory: 13.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    memo=[0 for _ in range(60001)]
+    if n==1:
+        return 1
+    memo[1]=1
+    memo[2]=2
+    for i in range(3,n+1):
+        memo[i] = (memo[i - 1] + memo[i - 2]) % 1000000007
+    return memo[n]
