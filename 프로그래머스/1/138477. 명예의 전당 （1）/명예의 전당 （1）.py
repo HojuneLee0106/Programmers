2674a35058d9d73174b31ae5cf0c45fef4b13fd1
HojuneLee <147648712+HojuneLee0106@users.noreply.github.com>
2026-09-19 21:17:10 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.20 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -0,0 +1,10 @@
+import heapq
+def solution(k, score):
+    answer = []
+    heap=[]
+    for i in range(len(score)):
+        heapq.heappush(heap,score[i])
+        if len(heap)>k:
+            heapq.heappop(heap)
+        answer.append(heap[0])
+    return answer
